2da201a9dacc110edfced21bb10b0b71be95d406
Greg Cedarblade <gmcedarblade@gmail.com>
2017-04-20 08:59:45 -0500

    created new layout for app

--- app/src/main/java/edu/cvtc/android/demonstratingfourtechniques/MainActivity.java
+++ app/src/main/java/edu/cvtc/android/demonstratingfourtechniques/MainActivity.java
@@ -7,6 +7,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_main);
+        
     }
 }
